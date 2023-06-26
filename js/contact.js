ee70fd7b50644d543382af9592c2dc9c706aab30
Jamie Conway <jamie@jpconway.com>
2023-06-26 05:46:13 -0500

    remove vanilla html, css, js files

--- js/contact.js
+++
@@ -1,4 +0,0 @@
-const header = document.getElementsByTagName('header');
-
-header[0].style.backgroundImage = "url(./images/nathan-dumlao-pnmRtTHWqDM-unsplash.jpg)";
-header[0].style.backgroundPositionY = 'bottom';
