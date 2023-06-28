0c2c4e38dcece78d6ee65c7a53139fdb500c9766
Jamie Conway <jamie@jpconway.com>
2023-06-28 15:17:06 -0500

    update styling of menu items

--- coffee-shop-v4/src/components/menu/MenuItem.jsx
+++ coffee-shop-v4/src/components/menu/MenuItem.jsx
@@ -1,3 +1,4 @@
+import zIndex from "@mui/material/styles/zIndex";
 import AddToCartButton from "../AddToCartButton";
 
 const MenuItem = ({ menuItem, cartVisible, toggleCartVisibility, addToCart }) => {
@@ -12,20 +13,64 @@ const MenuItem = ({ menuItem, cartVisible, toggleCartVisibility, addToCart }) =>
               flexDirection:'column',
               justifyContent:'center',
               alignItems:'center',
-              gap:'10px'
+              gap:'10px',
+              marginBottom:50
             }}
           >
-            <h2>{name}</h2>
-            <div>{price}</div>
-            <img
-              src={img}
-              alt="menu item"
+            <div
               style={{
                 width:400,
                 height:400,
-                objectFit:'cover'
+                position:'relative',
+                overflow:'hidden',
               }}
-            />
+            >
+              <h2
+                style={{
+                  position: 'absolute', // Added position property
+                  top: 100, // Positioned at the top
+                  left: 0, // Positioned at the left
+                  color: 'white', // Added text color
+                  margin: 0, // Remove default margin
+                  width: '100%', // Full width
+                  fontSize:'3.2rem',
+                  zIndex:9
+                }}
+              >{name}</h2>
+              <div
+                style={{
+                  position: 'absolute', // Added position property
+                  bottom: 0, // Positioned at the bottom
+                  width:'100%',
+                  backgroundColor: 'var(--dark-color)', // Added background color with opacity
+                  color: 'white', // Added text color
+                  padding: '8px 0', // Added padding
+                  margin: 0, // Remove default margin
+                  zIndex:9
+                }}
+              >{price}</div>
+              <div
+                style={{
+                  width:'100%',
+                  height:'100%',
+                  backgroundColor:'var(--dark-color)',
+                  opacity:'0.7',
+                  position:'absolute',
+                  top:0,
+                  left:0,
+                  zIndex:8
+                }}
+              ></div>
+              <img
+                src={img}
+                alt="menu item"
+                style={{
+                  width:'100%',
+                  height:'100%',
+                  objectFit:'cover',
+                }}
+              />
+            </div>
             <AddToCartButton
               addToCart={addToCart}
               menuItem={menuItem}
