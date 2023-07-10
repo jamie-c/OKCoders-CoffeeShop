0affce3c017e756e0a0abf1bd74c0c01ed37ecd2
Jamie Conway <jamie@jpconway.com>
2023-07-10 10:49:53 -0500

    create layout for react router

---
+++ coffee-shop-v4/src/components/Layout.jsx
@@ -0,0 +1,36 @@
+import Navbar from "./navbar/Navbar";
+import { Box, Stack } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import Footer from "./footer/Footer";
+import Cart from "./cart/Cart";
+
+console.log('rendering layout...');
+const Layout = ({ shoppingCart, addToCart, remFromCart, cartVisible, toggleCartVisibility, loggedIn }) => {
+
+    // if (loggedIn === false) {
+	// 	return <Navigate to="/login" />;
+	// }
+
+    return (
+    <Stack height='100%'>
+        <Navbar 
+            shoppingCart={shoppingCart}
+            addToCart={addToCart}
+            remFromCart={remFromCart} 
+            cartVisible={cartVisible}
+            toggleCartVisibility={toggleCartVisibility}
+        />
+        {cartVisible && 
+        <Cart 
+            shoppingCart={shoppingCart} 
+            remFromCart={remFromCart} 
+            addToCart={addToCart}
+        />}
+        <Box height='100%'>
+            <Outlet />
+        </Box>
+        <Footer />
+    </Stack>
+    );
+};
+export default Layout;
