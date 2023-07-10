33fb1e3b8fa4ba991d4af22862fdabe9d439a35f
Jamie Conway <jamie@jpconway.com>
2023-07-10 10:54:58 -0500

    add contact form

---
+++ coffee-shop-v4/src/components/navbar/Contact.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material';
+import ContactForm from '../ContactForm';
+
+const Contact = () => {
+    return (
+        <Stack height='100%'>
+            <ContactForm />
+        </Stack>
+    );
+};
+
+export default Contact;
