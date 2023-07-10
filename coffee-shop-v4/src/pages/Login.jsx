2973654cc39b788e3579cb3bee61f9ec0f7e7838
Jamie Conway <jamie@jpconway.com>
2023-07-10 10:55:15 -0500

    create login page

---
+++ coffee-shop-v4/src/pages/Login.jsx
@@ -0,0 +1,72 @@
+import {
+    Box,
+	Button,
+	Container,
+	FormControl,
+	FormHelperText,
+	Input,
+	InputLabel,
+	Stack,
+	Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = ({ loggedIn, setLoggedIn }) => {
+	const navigate = useNavigate();
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		console.log(formData);
+        setLoggedIn(true);
+		navigate('/menu');
+	};
+
+	return (loggedIn === false ? (
+		<Container>
+			<Stack component="form" onSubmit={handleSubmit} alignItems="center">
+				<Typography variant="h4">Login</Typography>
+				<FormControl fullWidth margin="normal">
+					<InputLabel htmlFor="email">Email address</InputLabel>
+					<Input
+						id="email"
+						aria-describedby="email-helper-text"
+						placeholder="email"
+						value={formData.email}
+						onChange={e => setFormData({ ...formData, email: e.target.value })}
+						fullWidth
+                        type='email'
+                        required
+					/>
+					<FormHelperText id="email-helper-text">
+						We'll never share your email.
+					</FormHelperText>
+				</FormControl>
+				<FormControl fullWidth margin="normal">
+					<InputLabel htmlFor="password">Password</InputLabel>
+					<Input
+						id="password"
+						aria-describedby="password-helper-text"
+						placeholder="password"
+						value={formData.password}
+						type="password"
+						onChange={e => setFormData({ ...formData, password: e.target.value })}
+						fullWidth
+                        required
+					/>
+				</FormControl>
+				<Button type="submit" variant="contained" color="primary" fullWidth>
+					Sign In
+				</Button>
+			</Stack>
+		</Container>
+        ) : (
+        <Box>Hello, you're signed in under the email: {formData.email}!</Box>)
+    );
+};
+
+export default Login;
